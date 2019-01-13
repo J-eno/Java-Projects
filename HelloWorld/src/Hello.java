2595a8d1b1c7ecb932635ca4340cc6f27ce1db60
JENO <uberjoel25@yahoo.com>
2019-01-13 18:05:30 -0500

    Added variables
    This Java course is starting off with very basic, beginner concepts that I already know from C++, C#, Python etc.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,7 +3,15 @@ public class Hello {
     public static void main(String[] args)
     {
         System.out.println("Hello, World...");
-        System.out.println("These tutorials are for absolute beginners, and I'm getting impatient.");
+
+        int myInt = 35;
+        int myInt2 = 12;
+        int myInt3 = myInt * 2;
+
+        int myTotal = myInt +myInt2 + myInt3;
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 
 
